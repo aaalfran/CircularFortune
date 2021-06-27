6f53231d5af49441af15a492bd8f3fb0b6c575da
aaalfran <aaalfran@espol.edu.ec>
2021-06-26 23:31:18 -0500

    Proyecto Base

---
+++ src/circularfortune/menuinicioController.java
@@ -0,0 +1,21 @@
+package circularfortune;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class menuinicioController {
+
+    @FXML
+    private ImageView fondo;
+
+    @FXML
+    private Label titulo;
+
+    @FXML
+    private Button play;
+
+}
+
+   
